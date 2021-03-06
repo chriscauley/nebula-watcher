7d15ae41368d3ed64584271c61206c96f9b7b4bc
chriscauley <chris@lablackey.com>
2021-03-06 14:02:12 +0000

    tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-<div class="app-trigger">
-  <img src="./assets/icon64.png" />
+<div class="app-trigger" @click="toggle">
+  <img src="../public/icons/icon64.png" />
+</div>
+<div class="modal" v-if="open">
+  <div class="modal-mask" @click="toggle" />
+  <div class="modal-content">
+    <div class="modal-close" @click="toggle" />
+    Woo!
+  </div>
 </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {open: false}
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open
+    }
+  }
 }
 </script>
